a867106ebce494015e7e88a11e4404b9433ed3e0
James Gale <james@Jamess-MacBook-Air.local>
2020-12-18 16:31:47 -0500

    db change

--- db/init_db.js
+++ db/init_db.js
@@ -7,46 +7,46 @@ const {
   getReportsByBranchDate,
 } = require("./index");
 
-// async function createTables() {
-//   try {
-//     await client.query(`
-//         CREATE TABLE reports (
-//           id SERIAL PRIMARY KEY,
-//           Submission varchar,
-//           Branch varchar,
-//           Technician varchar,
-//           Date date,
-//           Type varchar,
-//           Number varchar,
-//           Defects varchar,
-//           trailer varchar,
-//           Brakes varchar,
-//           Coupling varchar,
-//           Remarks varchar,
-//           Options varchar,
-//           Dsignature varchar,
-//           Msignature varchar,
-//           DrSignature varchar
-//         );
-//       `);
-//   } catch (error) {
-//     throw error;
-//   }
-// }
+async function createTables() {
+  try {
+    await client.query(`
+        CREATE TABLE reports (
+          id SERIAL PRIMARY KEY,
+          Submission varchar,
+          Branch varchar,
+          Technician varchar,
+          Date date,
+          Type varchar,
+          Number varchar,
+          Defects varchar,
+          trailer varchar,
+          Brakes varchar,
+          Coupling varchar,
+          Remarks varchar,
+          Options varchar,
+          Dsignature varchar,
+          Msignature varchar,
+          DrSignature varchar
+        );
+      `);
+  } catch (error) {
+    throw error;
+  }
+}
 
-// async function dropTables() {
-//   try {
-//     console.log("Starting to drop tables...");
-//     await client.query(`
-//     DROP TABLE if EXISTS reports;
-//       `);
+async function dropTables() {
+  try {
+    console.log("Starting to drop tables...");
+    await client.query(`
+    DROP TABLE if EXISTS reports;
+      `);
 
-//     console.log("Finished dropping tables!");
-//   } catch (error) {
-//     console.error("Error dropping tables!");
-//     throw error;
-//   }
-// }
+    console.log("Finished dropping tables!");
+  } catch (error) {
+    console.error("Error dropping tables!");
+    throw error;
+  }
+}
 
 async function rebuildDB() {
   try {
@@ -59,13 +59,13 @@ async function rebuildDB() {
 
 async function testDB() {
   try {
-    // await dropTables();
-    // await createTables();
-    const alls = await getReportsByBranchDate(
-      "2020-11-25",
-      "2020-11-25",
-      "ATLANTA"
-    );
+    await dropTables();
+    await createTables();
+    // const alls = await getReportsByBranchDate(
+    //   "2020-11-25",
+    //   "2020-11-25",
+    //   "ATLANTA"
+    // );
     // const ranges = await getReportsByDate("2020-11-02", "2020-12-02");
     // const branch = await getReportsByBranch("ATLANTA");
     console.log(alls);
